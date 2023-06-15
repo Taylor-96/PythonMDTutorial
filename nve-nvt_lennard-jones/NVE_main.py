aadd97864c63852eba0ddc3fd427df9af28d3ede
Taylor <taylor.baird@epfl.ch>
2023-06-15 10:09:30 +0200

    Allow specification of system parameters as arguments.

--- nve-nvt_lennard-jones/NVE_main.py
+++ nve-nvt_lennard-jones/NVE_main.py
@@ -1,2 +1,14 @@
+import argparse
 from NVE_lj import VV_mdlj
-VV_mdlj(mode=0,nstep=1000,freq=10,kt=1)
+parser = argparse.ArgumentParser()
+parser.add_argument("--kt",type=float,default=0.694,help='The temperature of the system.', required=False)
+parser.add_argument("--nstep",type=int,default=1000,help='Total number of timesteps to run the simulation for.', required=False)
+parser.add_argument("--mode",type=int,default=0,help='Initialization mode (see notes).'  , required=False)
+parser.add_argument("--dt",type=float,default=0.005,help='Size of timestep to use.'  , required=False)
+args = parser.parse_args()
+
+kt=args.kt
+nstep=args.nstep
+mode=args.mode
+dt=args.dt
+VV_mdlj(mode=mode,nstep=nstep,dt=dt,freq=10,kt=kt)
--- nve-nvt_lennard-jones/NVT_main.py
+++ nve-nvt_lennard-jones/NVT_main.py
@@ -1,2 +1,20 @@
+import argparse
 from NVT_lj import NH_mdlj
+parser = argparse.ArgumentParser()
+parser.add_argument("--kt",type=float,default=0.694,help='The temperature of the system.', required=False)
+parser.add_argument("--tausq",type=float,default=1.0,help='tau**2 parameter for N-H thermostat.', required=False)
+parser.add_argument("--kt_init",type=float,default=1.0,help='Initial temperature for the system.', required=False)
+parser.add_argument("--kt_targ",type=float,default=1.0,help='Target temperature for the system.', required=False)
+parser.add_argument("--nstep",type=int,default=1000,help='Total number of timesteps to run the simulation for.', required=False)
+parser.add_argument("--mode",type=int,default=0,help='Initialization mode (see notes).'  , required=False)
+parser.add_argument("--dt",type=float,default=0.005,help='Size of timestep to use.'  , required=False)
+args = parser.parse_args()
+
+kt_init=args.kt_init
+nstep=args.nstep
+mode=args.mode
+dt=args.dt
+tausq=args.tausq
+kt_targ=args.kt_targ
+
 NH_mdlj(mode=0,nstep=4000,kt_init=.5,kt_targ=1.,tausq=1.,freq=20)
